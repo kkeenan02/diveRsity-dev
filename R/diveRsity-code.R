8a9a872fa2ce8d429c600031e010d24750228d69
kkeenan02 <kkeenan02@qub.ac.uk>
2013-10-26 18:37:42 +0100

    diveRsity-dev push

--- R/diveRsity-code.R
+++ R/diveRsity-code.R
@@ -7726,22 +7726,22 @@ divMigrate <- function(infile = NULL, stat = "d_jost"){
 # ################################################################################
 # # pwDivCalc: a small function for calculating pairwise ht and hs 
 # ################################################################################
-# pwDivCalc <- function(x, pw, npops){
-#   ht <- matrix(ncol = npops, nrow = npops)
-#   hs <- matrix(ncol = npops, nrow = npops)
-#   for(i in 1:ncol(pw)){
-#     gamma <- sum(sqrt(abs(x[,1] * x[,2])))^-1 
-#     f <- gamma * sqrt(x[,pw[1,i]] * x[,pw[2,i]])
-#     ht[pw[1,i],pw[2,i]] <- 1 - sum(((f + x[,pw[1,i]])/2)^2)
-#     ht[pw[2,i],pw[1,i]] <- 1 - sum(((f + x[,pw[2,i]])/2)^2)
-#     hs[pw[1,i],pw[2,i]] <- 1 - sum((f^2 + x[,pw[1,i]]^2)/2)
-#     hs[pw[2,i],pw[1,i]] <- 1 - sum((f^2 + x[,pw[2,i]]^2)/2)
-#   }
-#   ht[is.nan(ht)] <- 0
-#   hs[is.nan(hs)] <- 0
-#   list(ht = ht, 
-#        hs = hs)
-# }
+pwDivCalc <- function(x, pw, npops){
+  ht <- matrix(ncol = npops, nrow = npops)
+  hs <- matrix(ncol = npops, nrow = npops)
+  for(i in 1:ncol(pw)){
+    gamma <- sum(sqrt(abs(x[,1] * x[,2])))^-1 
+    f <- gamma * sqrt(x[,pw[1,i]] * x[,pw[2,i]])
+    ht[pw[1,i],pw[2,i]] <- 1 - sum(((f + x[,pw[1,i]])/2)^2)
+    ht[pw[2,i],pw[1,i]] <- 1 - sum(((f + x[,pw[2,i]])/2)^2)
+    hs[pw[1,i],pw[2,i]] <- 1 - sum((f^2 + x[,pw[1,i]]^2)/2)
+    hs[pw[2,i],pw[1,i]] <- 1 - sum((f^2 + x[,pw[2,i]]^2)/2)
+  }
+  ht[is.nan(ht)] <- 0
+  hs[is.nan(hs)] <- 0
+  list(ht = ht, 
+       hs = hs)
+}
 # ################################################################################
 # # END - pwDivCalc
 # ################################################################################
